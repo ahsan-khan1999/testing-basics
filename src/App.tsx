9b7193b8a839307684e1c636eed564d8a28c09d2
ahsan-khan1999 <ahsan.ak943@gmail.com>
2020-09-30 16:39:21 +0500

    basic test with jest

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  
+        <a>
+          Learn React
+        </a>
+        <input placeholder='press me'  style={{height:80,width:150}}/>
+      
+    </div>
+  );
+}
+
+export default App;
