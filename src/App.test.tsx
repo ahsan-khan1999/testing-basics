778b934022c37eec394357589027083e8364b1db
ahsan-khan1999 <ahsan.ak943@gmail.com>
2020-09-30 19:05:07 +0500

    setting first test with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+// test('another test',() => {
+//   const {getByPlaceholderText} = render(<App />)
+//   const element = getByPlaceholderText(/PRESS me/i);
+//   expect(element).toBeInTheDocument();
+// })
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+test('renders the component', () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
 });
-test('another test',() => {
-  const {getByPlaceholderText} = render(<App />)
-  const element = getByPlaceholderText(/PRESS me/i);
-  expect(element).toBeInTheDocument();
-})
